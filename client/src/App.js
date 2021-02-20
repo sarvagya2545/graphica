12c336aae57110527430571a68e31b949df1f69a
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-02-20 17:10:26 +0530

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      Halo
+    </>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+  res.send('Changes Work')
+})
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
