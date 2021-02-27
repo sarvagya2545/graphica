99705e25eeb11375a1caa3c42abf6cf262a5f0f3
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-02-27 15:41:00 +0530

    debug

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -10,7 +10,7 @@ const store = createStore(
   initState, 
   compose(
     applyMiddleware(...middleware),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
