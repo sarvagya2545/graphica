ee51d3e58597f885ea7c0bcf69c0d602d00a63e5
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-02-27 16:42:39 +0530

    d

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -15,7 +15,7 @@ export const loadUser = () => (dispatch) => {
   axios
     .get("/api/users/current", {
       withCredentials: true,
-      headers: { "content-type": "application/json", "Accept": "application/json" },
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
     })
     .then((res) => {
       console.log(res);
