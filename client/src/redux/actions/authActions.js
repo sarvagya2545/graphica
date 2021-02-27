7e3af8e4f0ec7682512f37e4160e6349565d6ee5
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-02-27 16:19:52 +0530

    deploy

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -11,21 +11,28 @@ import {
   USER_LOADED,
 } from "./types";
 
-export const loadUser = () => dispatch => {
-  axios.get('/api/users/current', { withCredentials: true, headers: { 'Content-Type': 'application/json' } })
-    .then(res => {
+export const loadUser = () => (dispatch) => {
+  axios
+    .get("/api/users/current", {
+      withCredentials: true,
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+    })
+    .then((res) => {
       console.log(res);
-      dispatch({ type: USER_LOADED, payload: res.data })
+      dispatch({ type: USER_LOADED, payload: res.data });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      dispatch({ type: NO_USER_LOADED })
-    })
-}
+      dispatch({ type: NO_USER_LOADED });
+    });
+};
 
 export const register = (formData) => (dispatch) => {
   axios
-    .post(`/api/users/signup`, formData, { withCredentials: true, headers: { 'Content-Type': 'application/json' } })
+    .post(`/api/users/signup`, formData, {
+      withCredentials: true,
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+    })
     .then((res) => {
       console.log(res);
       dispatch({ type: REGISTER_SUCCESS, payload: res.data });
@@ -50,7 +57,10 @@ export const register = (formData) => (dispatch) => {
 
 export const login = (formData) => (dispatch) => {
   axios
-    .post(`/api/users/login`, formData, { withCredentials: true, headers: { 'Content-Type': 'application/json' } })
+    .post(`/api/users/login`, formData, {
+      withCredentials: true,
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+    })
     .then((res) => {
       console.log(res);
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
@@ -64,8 +74,7 @@ export const login = (formData) => (dispatch) => {
         // unauthorized
         const errorData = {
           errors: {
-            email:
-              "Email or password entered is incorrect",
+            email: "Email or password entered is incorrect",
           },
           status: 401,
           errType: "Login error",
@@ -86,11 +95,16 @@ export const login = (formData) => (dispatch) => {
     });
 };
 
-export const logout = () => dispatch => {
-  axios.post(`/api/users/logout`, {}, { withCredentials: true, headers: { 'Content-Type': 'application/json' } })
-    .then(res => {
+export const logout = () => (dispatch) => {
+  axios
+    .post(
+      `/api/users/logout`,
+      {},
+      { withCredentials: true, headers: { "Content-Type": "application/json", "Accept": "application/json" } }
+    )
+    .then((res) => {
       console.log(res);
-      dispatch({ type: LOGOUT_SUCCESS })
+      dispatch({ type: LOGOUT_SUCCESS });
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
