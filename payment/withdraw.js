cfbda4e073dda395353a9d36769050310af91a3a
unknown <pjajith010@gmail.com>
2021-03-15 13:26:13 +0530

    fixed cognitive complexity in withdraw.js

--- payment/withdraw.js
+++ payment/withdraw.js
@@ -16,54 +16,47 @@ const withdraw = function(app){
         }
     });
 
-    app.get('/withdraw',passport.authenticate('jwt', { session: false }),(req,res)=>{
-        res.send('<form action="/withdraw" method="post"> <input type="text" name="email"><input type="text" name="amount"><input type="submit" value="Withdraw"></form> ');
-    });
-
     app.post("/withdraw", passport.authenticate('jwt', { session: false }), (req, res) =>{
 
         Designer.findById(req.user._id,function(err,designer){
             if(err){
                 console.log(err);
-            }else{
-                console.log(req.body.amount,designer.balance);
-                if(designer.balance>=req.body.amount&&req.body.amount>0){
-                    var sender_batch_id = Math.random().toString(36).substring(9);
-                    var create_payout_json = {
-                        "sender_batch_header": {
-                            "sender_batch_id": sender_batch_id,
-                            "email_subject": "Graphica"
-                        },
-                        "items": [
-                            {
-                                "recipient_type": "EMAIL",
-                                "amount": {
-                                    "value": req.body.amount,
-                                    "currency": "USD"
-                                },
-                                "receiver": req.body.email,
-                                "note": "Thank you.",
-                            }
-                        ]
-                    };
-                    var sync_mode = 'false';
-                    paypal.payout.create(create_payout_json, sync_mode, function (error, payout) {
-                        if (error) {
-                            console.log(error.response);
-                            throw error;
-                        } else {
-                            res.send('Success');
-                            let updatedBalance=designer.balance-req.body.amount;
-                            Designer.updateOne( {_id:req.user._id},{balance: updatedBalance},function(ERR,RES){
-                                if(ERR){
-                                  console.log(RES);
-                                }
-                              });
+            }else if(designer.balance>=req.body.amount&&req.body.amount>0){
+                var sender_batch_id = Math.random().toString(36).substring(9);
+                var create_payout_json = {
+                    "sender_batch_header": {
+                        "sender_batch_id": sender_batch_id,
+                        "email_subject": "Graphica"
+                    },
+                    "items": [
+                        {
+                            "recipient_type": "EMAIL",
+                            "amount": {
+                                "value": req.body.amount,
+                                "currency": "USD"
+                            },
+                            "receiver": req.body.email,
+                            "note": "Thank you.",
                         }
-                    });
-                }else{
-                    res.status(400).json({err:'Insufficient Balance'});
-                }
+                    ]
+                };
+                var sync_mode = 'false';
+                paypal.payout.create(create_payout_json, sync_mode, function (error, payout) {
+                    if (error) {
+                        console.log(error.response);
+                        throw error;
+                    } else {
+                        res.send('Success');
+                        let updatedBalance=designer.balance-req.body.amount;
+                        Designer.updateOne( {_id:req.user._id},{balance: updatedBalance},function(ERR,RES){
+                            if(ERR){
+                                console.log(RES);
+                            }
+                        });
+                    }
+                });
+            }else{
+                res.status(400).json({err:'Insufficient Balance'});
             }
         });
     });
